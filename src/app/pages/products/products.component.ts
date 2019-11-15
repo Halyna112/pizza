329dcd6c112f2b61b62b06bc8e3f36fb91ac206f
Halyna112 <smetanahalyna112@gmail.com>
2019-11-15 19:31:44 +0200

    my commit

---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/interfaces/product.interface';
+import { ProductsService } from 'src/app/shared/services/products.service';
+import { Product } from 'src/app/shared/classes/product.model';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  products: Array<IProduct> = [];
+
+  constructor(private productsService: ProductsService) {
+    this.getProducts();
+   }
+
+  ngOnInit() {
+  }
+
+  private getProducts(): void {
+    this.productsService.getJsonProducts().subscribe(
+      data => {
+        this.products = data;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  // getPageProducts(): void {
+  //   this.products = this.productsService.getProducts();
+  // }
+
+}
